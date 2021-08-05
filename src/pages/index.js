f24554a2a5c107c20a804206a6753f9a0ccee07a
Laze <lazeto8@gmail.com>
2021-08-05 13:01:41 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Layout from '../components/Layout'
+import Posts from '../components/Posts'
+import { graphql } from 'gatsby'
+import SEO from '../components/SEO'
+
+const IndexPage = ({
+  data: {
+    allMdx: { nodes: posts },
+  },
+}) => {
+  console.log(posts)
+  return (
+    <Layout>
+      <Hero showPerson />
+      <Posts posts={posts} title="recently published" />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }, limit: 3) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          date(formatString: "MMMM Do, YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
+export default IndexPage
